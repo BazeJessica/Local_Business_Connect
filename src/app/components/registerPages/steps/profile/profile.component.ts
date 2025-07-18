2698873e4649659c5f90b3ca38738607e499dc83
Jessica <jessicaennor@gmail.com>
2025-07-18 17:55:38 +0200

    feat: modify business dashboard, calender, customers, and profile component folder

--- src/app/components/registerPages/steps/profile/profile.component.ts
+++ src/app/components/registerPages/steps/profile/profile.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  profile = [
+    { day: 'Monday', hours: '9:00 AM - 7:00 PM' },
+    { day: 'Tuesday', hours: '9:00 AM - 7:00 PM' },
+    { day: 'Wednesday', hours: '9:00 AM - 7:00 PM' },
+    { day: 'Thursday', hours: '9:00 AM - 7:00 PM' },
+    { day: 'Friday', hours: '9:00 AM - 8:00 PM' },
+    { day: 'Saturday', hours: '8:00 AM - 6:00 PM' },
+    { day: 'Sunday', hours: 'Closed' },
+  ]
 
+  reviews = [
+    { stars: 5, count: 89, percentage: 70 },
+    { stars: 4, count: 28, percentage: 22 },
+    { stars: 3, count: 7, percentage: 6 },
+    { stars: 2, count: 2, percentage: 2 },
+    { stars: 1, count: 1, percentage: 1 },
+  ]
 }
